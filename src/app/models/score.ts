a20bd0c6e4256dfc6498e997ccb717afdbfcd53d
AppledoreProductions <appledore.productions@gmail.com>
2019-03-04 12:44:47 +0000

    work on importing scrape scores from data.fei.org

---
+++ src/app/models/score.ts
@@ -0,0 +1,25 @@
+export class Score {
+    private _rider: string;
+    private _horse: string;
+    private _dressage: number;
+    private _xcfault: number;
+    private _xctime: number;
+    private _sjfault: number;
+    private _sjtime: number;
+    private _jumpofffault: number;
+    private _jumpofftime: number;
+
+    constructor(rider: string, horse: string, scores: string) {
+        this._rider = rider;
+        this._horse = horse;
+        const re: RegExp = new RegExp('^\\t?([\\d\\.]*?)\\s+([\\d\\.]*?)\\s+([\\d\\.]*?)\\s+([\\d\\.]*?)\\s+([\\d\\.]*?)\\s+([\\d\\.]*?)\\s+([\\d\\.]*?)\\s+([\\d\\.]*?)\\s+([\\d\\.]*?)\\s+(.*?)$', 'g');
+        let matches = re.exec(scores);
+        this._dressage = Number(matches[1]);
+        this._xcfault = Number(matches[2]);
+        this._xctime = Number(matches[3]);
+        this._sjfault = Number(matches[4]);
+        this._sjtime = Number(matches[5]);
+        this._jumpofffault = Number(matches[6]);
+        this._jumpofftime = Number(matches[7]);
+    }
+}
