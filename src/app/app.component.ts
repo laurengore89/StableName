13c657fa5c962438c0b8fc17cb7f3ff56d0a8f86
AppledoreProductions <appledore.productions@gmail.com>
2019-03-06 16:55:32 +0000

    this went smoothly, it works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent {
   private readonly patternBadminton = '^\\t?([\\d\\.]*?)\\s+([\\d\\.]*?)\\s+([\\d\\.]*?)\\s+([\\d\\.]*?)\\s+([\\d\\.]*?)\\s+([\\d,\\.]*?\\sGBP)?\\s+\\s+(.*?)$';
 
   constructor(private http: HttpClient) {
-    this.datablock = new Datablock(http, '', '', '');
-    // this.datablock = new Datablock(http, 'assets/input.txt', '2017_CI_0500_C_S_02_01', '2017 Bramham', this.patternBadminton);
+    this.datablock = new Datablock(http, '', '', '', '');
+    // this.datablock = new Datablock(http, 'assets/input.txt', '2018_CI_0408_C_S_01_01', '2018 Badminton', this.patternBadminton);
   }
 }
