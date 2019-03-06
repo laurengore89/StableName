f81a09f9e5daa154177d980d0df419de35299b8c
AppledoreProductions <appledore.productions@gmail.com>
2019-03-06 16:53:24 +0000

    missed a bit

---
+++ src/app/models/competition.ts
@@ -0,0 +1,9 @@
+export class Competition {
+    public Fei: string;
+    public Name: string;
+
+    constructor(fei: string, name: string) {
+        this.Fei = fei;
+        this.Name = name;
+    }
+}
