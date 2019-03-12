0596b2d5c6a12cf4bbbaf9478872129056929d80
laurengore89 <laurengore89@gmail.com>
2019-03-12 21:24:13 +0000

    display as rider strings instead

--- src/app/components/horse/horse.component.ts
+++ src/app/components/horse/horse.component.ts
@@ -1,22 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Horse, Datablock, Rider } from '../../models';
+import { Component, Input } from '@angular/core';
+import { Horse } from '../../models';
 
 @Component({
   selector: 'sn-horse',
   templateUrl: './horse.component.html',
   styleUrls: ['./horse.component.scss']
 })
-export class HorseComponent implements OnInit {
+export class HorseComponent {
   @Input() horse: Horse;
-  @Input() datablock: Datablock;
-  riders: Rider[];
-
-  ngOnInit(): void {
-    this.riders = [];
-    this.datablock.scores.filter(s => s.Horse.Fei === this.horse.Fei).map(s => s.Rider.Fei).forEach(f => {
-      if (this.riders.find(r => r.Fei === f) === undefined) {
-        this.riders.push(this.datablock.riders.find(r => r.Fei === f));
-      }
-    });
-  }
 }
--- src/app/components/rider/rider.component.ts
+++ src/app/components/rider/rider.component.ts
@@ -1,12 +1,23 @@
-import { Component, Input } from '@angular/core';
-import { Rider } from '../../models';
+import { Component, Input, OnInit } from '@angular/core';
+import { Rider, Datablock, Horse } from '../../models';
 
 @Component({
   selector: 'sn-rider',
   templateUrl: './rider.component.html',
   styleUrls: ['./rider.component.scss']
 })
-export class RiderComponent {
+export class RiderComponent implements OnInit {
   @Input() rider: Rider;
+  @Input() datablock: Datablock;
+  horses: Horse[];
+
+  ngOnInit(): void {
+    this.horses = [];
+    this.datablock.scores.filter(s => s.Rider.Fei === this.rider.Fei).map(s => s.Horse.Fei).forEach(f => {
+      if (this.horses.find(h => h.Fei === f) === undefined) {
+        this.horses.push(this.datablock.horses.find(h => h.Fei === f));
+      }
+    });
+  }
 }
 
