6c1eef8618b9cae34bdb68aa54f892f7d7094569
laurengore89 <laurengore89@gmail.com>
2019-03-13 13:23:08 +0000

    tweak tweak

--- src/app/enums/regexpattern.ts
+++ src/app/enums/regexpattern.ts
@@ -5,7 +5,7 @@ export enum RegexPattern {
 
     // 	38	0	2.4	0	1	100,000 GBP		41.4
     // dressage | XC | SJ | prize money | final total
-    Eventing = '^\\t?([\\d\\.]*?)\\s+([\\d\\.]*?)\\s+([\\d\\.]*?)\\s+([\\d\\.]*?)\\s+([\\d\\.]*?)\\s+([\\d,\\.]*?\\s[GBPEUR])?\\s+\\s+(.*?)$',
+    Eventing = '^\\t?([\\d\\.]*?)\\s+([\\d\\.]*?)\\s+([\\d\\.]*?)\\s+([\\d\\.]*?)\\s+([\\d\\.]*?)\\s+([\\d,\\.]*?\\s[A-Z]{3})?\\s+\\s+(.*?)$',
 
     //  85.100	84.500	86.900	86.800	85.300	82.900	84.000		85.071
     // 7 letter judges | final average
@@ -17,5 +17,5 @@ export enum RegexPattern {
 
     // 2,700 EUR	77.900	78.200	77.100	76.300	77.800		77.460
     // prize money | 5 letter judges | final average
-    Dressage5 = '^\\s*[\\d\\.]*?\\s[GBPEUR]*\\s+[\\d\\.]*?\\s+[\\d\\.]*?\\s+[\\d\\.]*?\\s+[\\d\\.]*?\\s+[\\d\\.]*?\\s+([\\d\\.]*?)$'
+    Dressage5 = '^\\s*[\\d,]*?\\s[A-Z]{3}\\s+[\\d\\.]*?\\s+[\\d\\.]*?\\s+[\\d\\.]*?\\s+[\\d\\.]*?\\s+[\\d\\.]*?\\s+([\\d\\.]*?)$'
 }
