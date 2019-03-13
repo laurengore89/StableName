fc30bfe90288b1d80e0b1df81e6434f6d8397166
laurengore89 <laurengore89@gmail.com>
2019-03-13 16:07:10 +0000

    more jumping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,6 @@ export class AppComponent {
 
   constructor(private http: HttpClient) {
     this.datablock = new Datablock(http, '', '', '', '');
-    //this.datablock = new Datablock(http, 'assets/input.txt', '2016_CI_0273_S_S_01_07', '2016 Furusiyya FEI Nations Cup Jumping Final', RegexPattern.Jumping);
+    //this.datablock = new Datablock(http, 'assets/input.txt', '2015_CI_0074_S_S_01_03', '2015 Cannes Longines Global Champions Tour Grand Prix', RegexPattern.Jumping);
   }
 }
