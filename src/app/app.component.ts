c6bcb56de0a3c169fdb16187c6c9396161792a2e
laurengore89 <laurengore89@gmail.com>
2019-03-13 13:37:01 +0000

    test new pattern

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,6 @@ export class AppComponent {
 
   constructor(private http: HttpClient) {
     this.datablock = new Datablock(http, '', '', '', '');
-    //this.datablock = new Datablock(http, 'assets/input.txt', '2008_OG_0001_D_S_01_01', '2008 Beijing Olympic Dressage Grand Prix', RegexPattern.DressageOlympic5);
+    //this.datablock = new Datablock(http, 'assets/input.txt', '2017_CI_0288_D_S_01_01', '2017 Hickstead GP', RegexPattern.Dressage);
   }
 }
