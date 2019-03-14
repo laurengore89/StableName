2acd3d0a13e283e4abc4c5f6a833f266b3f685f9
laurengore89 <laurengore89@gmail.com>
2019-03-14 12:29:58 +0000

    sort them by ID for want of a better idea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,6 @@ export class AppComponent {
   constructor(private http: HttpClient) {
     this.datablock = new Datablock(http, '', '', '', '');
     //this.datablock = new Datablock(http, 'assets/input.txt', '2015_CI_0074_S_S_01_03', '2015 Cannes Longines Global Champions Tour Grand Prix', RegexPattern.Jumping);
-    this.ridersShow = this.datablock.riders.filter(rider => this.datablock.scores.some(s => s.Rider.Fei === rider.Fei));
+    this.ridersShow = this.datablock.riders.filter(rider => this.datablock.scores.some(s => s.Rider.Fei === rider.Fei)).sort((a, b) => { if (a.Fei > b.Fei) { return 1; } if (a.Fei < b.Fei) { return -1; } return 0; });
   }
 }
