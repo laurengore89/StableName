1f8cdc72067e349fa67a33e143afd2b005438d81
laurengore89 <laurengore89@gmail.com>
2019-03-15 13:25:48 +0000

    auto search

--- src/app/components/horse/horse.component.ts
+++ src/app/components/horse/horse.component.ts
@@ -6,6 +6,15 @@ import { Horse } from '../../models';
   templateUrl: './horse.component.html',
   styleUrls: ['./horse.component.scss']
 })
+
 export class HorseComponent {
   @Input() horse: Horse;
+  @Input() ridername: string;
+
+  openDataPages(): void {
+    window.open('https://data.fei.org/Horse/Performance.aspx?horsefeiid=' + this.horse.Fei);
+    window.open('https://www.google.co.uk/search?btnG=Search+Images&tbm=isch&q=' + this.horse.Name + '+' + this.ridername);
+    window.open('https://www.google.co.uk/search?q=' + this.horse.Name + '+' + this.ridername);
+    window.open('https://www.google.co.uk/search?q=' + this.horse.Name + '+' + 'British+Eventing');
+  }
 }
