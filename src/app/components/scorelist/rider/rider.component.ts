29faa97576f642f91cb442c83de1f7191d8c0252
laurengore89 <laurengore89@gmail.com>
2019-04-28 12:09:25 +0100

    italicise eliminated rider names

--- src/app/components/scorelist/rider/rider.component.ts
+++ src/app/components/scorelist/rider/rider.component.ts
@@ -1,15 +1,21 @@
-import { Component, Input } from '@angular/core';
-import { Rider, Horse } from 'src/app/models';
+import { Component, Input, OnInit } from '@angular/core';
+import { Rider } from 'src/app/models';
 
 @Component({
   selector: 'sn-scorelist-rider',
   templateUrl: './rider.component.html',
   styleUrls: ['./rider.component.scss']
 })
-export class ScorelistRiderComponent {
+export class ScorelistRiderComponent implements OnInit {
   @Input() draw: number;
   @Input() sum: string;
+  @Input() position: string;
   @Input() rider: Rider;
+  public dnf: boolean;
 
   constructor() { }
+
+  ngOnInit() {
+    this.dnf = this.position === 'EL' || this.position === 'WD' || this.position === 'RET' || this.position === 'DSQ';
+  }
 }
