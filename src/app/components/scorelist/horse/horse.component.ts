d3481fe44bccc4a4479f8b57a80856975a5b380e
laurengore89 <laurengore89@gmail.com>
2019-05-05 11:22:53 +0100

    re-style scores and give them room

--- src/app/components/scorelist/horse/horse.component.ts
+++ src/app/components/scorelist/horse/horse.component.ts
@@ -25,7 +25,7 @@ export class ScorelistHorseComponent implements OnInit {
 
     let scores: Score[] = this.horse.scores.filter(s => s.Rider.Fei === this.riderfei && s.Result.p !== 'PEND').sort((a, b) => { if (a.Competition.Fei > b.Competition.Fei) { return -1; } else if (b.Competition.Fei > a.Competition.Fei) { return 1; } return 0; });
 
-    this.scoresShow = scores.map(s => this.eventserieses.find(e => e.Id === this.comps.find(c => c.Fei === s.Competition.Fei).EventSeries).Name + '\xa0' + this.comps.find(c => c.Fei === s.Competition.Fei).Year + ':' + (s.Result.p === 'EL' || s.Result.p === 'WD' || s.Result.p === 'RET' || s.Result.p === 'DSQ' ? s.Result.p + '\xa0-\xa0' + s.Result.o : s.Result.p));
+    this.scoresShow = scores.map(s => this.eventserieses.find(e => e.Id === this.comps.find(c => c.Fei === s.Competition.Fei).EventSeries).Name + '\xa0' + this.comps.find(c => c.Fei === s.Competition.Fei).Year + ':' + (s.Result.p === 'EL' || s.Result.p === 'WD' || s.Result.p === 'RET' || s.Result.p === 'DSQ' ? s.Result.p + '\xa0-\xa0' + s.Result.o : s.Result.p + ': ' + s.Result.a + ' | ' + (s.Result.b + s.Result.c) + ' | ' + (s.Result.d + s.Result.e + (s.Result.f === undefined ? 0 : s.Result.f) + (s.Result.g === undefined ? 0 : s.Result.g))));
   }
 
   openDataPages(): void {
