516b921b5f60f3ce037128d3858c640b0a693449
laurengore89 <laurengore42@gmail.com>
2019-08-28 17:01:13 +0100

    WHERE DOES BINARY DATA GO? IN SOURCE CONTROL

--- src/app/models/horse.ts
+++ src/app/models/horse.ts
@@ -40,8 +40,9 @@ export class HorseDTO {
     public b?: string; // studbook, per Studbook enum
     public z?: number; // deceased
     public a?: string; // notes
+    public p?: string; // photograph, as base64 JPG
 
-    constructor(regdName: string, fei: string, stableName: string, sex: string, height: number, foaled: number, colour: string, legs: string, studbook: string, deceased: number, notes: string) {
+    constructor(regdName: string, fei: string, stableName: string, sex: string, height: number, foaled: number, colour: string, legs: string, studbook: string, deceased: number, notes: string, photo: string) {
         this.n = regdName;
         this.f = fei;
         this.s = stableName;
@@ -53,6 +54,7 @@ export class HorseDTO {
         this.b = studbook;
         this.z = deceased;
         this.a = notes;
+        this.p = photo;
     }
 }
 
@@ -68,6 +70,7 @@ export class Horse {
     private _studbook?: string;
     private _deceased?: number;
     private _notes?: string;
+    private _photo?: string;
     private _dto: HorseDTO;
 
     public scores: Score[];
@@ -86,6 +89,7 @@ export class Horse {
         this._studbook = dto.b;
         this._deceased = dto.z;
         this._notes = dto.a;
+        this._photo = dto.p;
     }
 
     get Dto(): HorseDTO {
@@ -152,6 +156,13 @@ export class Horse {
             return '';
         }
         return this._notes;
+    }
+
+    get Photo(): string {
+        if (!this._photo) {
+            return '';
+        }
+        return this._photo;
     }
 
     get Nearfore(): string {
